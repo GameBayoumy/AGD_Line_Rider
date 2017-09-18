c03489e3a5cec6b9972136df8b011b6aeadac23f
Sharif Bayoumy :3 <XilionHero@gmail.com>
2017-09-18 12:25:22 +0200

    [Change] Wall script to find player with a tag

--- AGD - Line Rider/Assets/Scripts/WallMesh.cs
+++ AGD - Line Rider/Assets/Scripts/WallMesh.cs
@@ -3,28 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class WallMesh : MonoBehaviour {
+public class WallMesh : MonoBehaviour
+{
 
     public GameObject Player;
-    public GameObject Wall;
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // movement = Player.transform.position.x;
-
-        Wall.transform.position = new Vector3(Player.transform.position.x, Wall.transform.position.y, Wall.transform.position.z);
-
-
+        transform.position = new Vector3(Player.transform.position.x, transform.position.y, transform.position.z);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player"){
+        if (collision.gameObject.tag == "Player")
+        {
             SceneManager.LoadScene("LineMeshTest");
         }
     }
