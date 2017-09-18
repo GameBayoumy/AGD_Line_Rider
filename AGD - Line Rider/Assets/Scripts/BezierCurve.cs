91ddbd2e1eef31b6e8879a8af70da60046bc3810
Sharif Bayoumy :3 <XilionHero@gmail.com>
2017-09-18 10:21:03 +0200

    [Adds] Bezier curve class

---
+++ AGD - Line Rider/Assets/Scripts/BezierCurve.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierCurve : MonoBehaviour
+{
+    public Vector3[] points;
+
+    public void Reset()
+    {
+        points = new Vector3[] {
+            new Vector3(1f, 0f, 0f),
+            new Vector3(2f, 0f, 0f),
+            new Vector3(3f, 0f, 0f)
+        };
+    }
+}
