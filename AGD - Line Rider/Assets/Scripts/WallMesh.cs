4cfc15566f04108c80ef6a546c2f47bce971907e
Michael Rensen <michael@illi-engineering.nl>
2017-09-18 11:52:21 +0200

    Wall now moves with the player.

--- AGD - Line Rider/Assets/Scripts/WallMesh.cs
+++ AGD - Line Rider/Assets/Scripts/WallMesh.cs
@@ -5,15 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class WallMesh : MonoBehaviour {
 
+    public GameObject Player;
+    public GameObject Wall;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement = Player.transform.position.x;
+
+        Wall.transform.position = new Vector3(Player.transform.position.x, Wall.transform.position.y, Wall.transform.position.z);
+
+
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
