11d758c6469534a14331704c412bebb4efc1982a
Michael Rensen <michael@illi-engineering.nl>
2017-09-19 11:41:13 +0200

    Small improvements

---
+++ AGD - Line Rider/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public float speed;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        
+	}
+
+	private void FixedUpdate()
+	{
+		float moveHorizontal = 1;
+		float moveVertical = 0;
+
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Debug.Log(speed);
+		rb2d.AddForce(movement * speed);
+
+		if (rb2d.velocity.x > 10)
+		{
+			rb2d.velocity = rb2d.velocity.normalized * 10;
+		}
+
+	}
+}
