e280887d646257bbc6213abd58a63395490a8729
Michael Rensen <michael@illi-engineering.nl>
2017-09-21 13:14:56 +0200

    The player now moves towards the left if the speed is lower then 8. Player can now lose if out of screenborder. Player can recover when having speed higher then 8.

--- AGD - Line Rider/Assets/Scripts/CameraController.cs
+++ AGD - Line Rider/Assets/Scripts/CameraController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour {
 
 
 	public GameObject Player;       //Public variable to store a reference to the player game object
-	private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+	public Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
 
 
-    // Use this for initialization
+	private void Awake()
+	{
+		Player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+
+	// Use this for initialization
 	void Start () {
 		//Calculate and store the offset value by getting the distance between the player's position and camera's position.
 		offset = transform.position - Player.transform.position;
@@ -18,7 +25,23 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Player.GetComponent<Rigidbody2D>().velocity.x <= 8){
+            offset += new Vector3(0.02f,0,0);
+
+            Debug.Log(offset);
+        }
+
+        if (offset.x >= 23){
+            SceneManager.LoadScene("LineMeshTest");
+        }
+
+        if(Player.GetComponent<Rigidbody2D>().velocity.x >= 8){
+            offset -= new Vector3(0.02f, 0, 0);
+        }
+
+        if (offset.x <= 0){
+            offset = new Vector3(0, 0, -10);
+        }
 	}
 
 	// LateUpdate is called after Update each frame
