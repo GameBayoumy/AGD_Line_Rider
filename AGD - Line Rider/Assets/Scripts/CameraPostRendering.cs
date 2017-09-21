ea4c5542afa1f88d0b4751b8320a845454321ad3
Sharif Bayoumy :3 <XilionHero@gmail.com>
2017-09-21 12:26:24 +0200

    [Adds] Triangle mosaic and painting shader effect

---
+++ AGD - Line Rider/Assets/Scripts/CameraPostRendering.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraPostRendering : MonoBehaviour
+{
+
+    public Material Mat;
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, Mat);
+    }
+}
