2c3db6f684a5847321331bd2b756c8298672832c
Sharif Bayoumy :3 <XilionHero@gmail.com>
2017-09-24 21:21:31 +0200

    [Add] Resource meter for drawing lines

--- AGD - Line Rider/Assets/Scripts/Player.cs
+++ AGD - Line Rider/Assets/Scripts/Player.cs
@@ -1,38 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
 	public float speed;
     public float maxResource;
-	public Rigidbody2D rb2d;
     public Touch touch;
+    public Text uiText;
+    public Slider uiSlider;
 
     [SerializeField]
-    private float drawResource = 1000;
-
+    private float drawResource;
+    private Rigidbody2D rb2d;
 	// Use this for initialization
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
+        drawResource = maxResource;             // Set start draw resource to the max resource
+        uiSlider.maxValue = maxResource;        // Set max value in ui slider
+        uiSlider.value = drawResource;          // Set current ui value to draw resource
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+        // Only changes resources within the min and max range
         if (touch.isDrawing)
         {
-            drawResource--;
-            touch.canDraw = true;
+            if(drawResource > 0)
+                drawResource--;
         }
-        else
+        else if(!touch.isDrawing)
         {
-            if(drawResource <= maxResource)
+            if(drawResource < maxResource)
                 drawResource++;
-            touch.canDraw = false;
         }
-	}
+
+        // Update UI elements
+        uiSlider.value = drawResource;
+        uiText.text = "Draw " + drawResource;
+
+        // Stop drawing when resource is empty
+        if(drawResource <= 0)
+            touch.canDraw = false;
+        else
+            touch.canDraw = true;
+    }
 
 	private void FixedUpdate()
 	{
--- AGD - Line Rider/Assets/Scripts/Touch.cs
+++ AGD - Line Rider/Assets/Scripts/Touch.cs
@@ -8,6 +8,7 @@ public class Touch : MonoBehaviour
     public GameObject currentMeshGenerator;         // this is the current line mesh generator
     public bool canDraw = true;
     public bool isDrawing;
+    public bool isTouching;
 
     private Vector3 pos;
     private Transform lineRend;
@@ -20,27 +21,44 @@ public class Touch : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
 
-        // Instantiate and set the current mesh generator on button down
         if (Input.GetMouseButtonDown(0))
         {
-            isDrawing = true;
-            pos.z = 0;
-            currentMeshGenerator = Instantiate(meshGeneratorPrefab, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
-            lineRend = currentMeshGenerator.transform;
+            isTouching = true; // Set touching to true to track player input
+            if (canDraw)
+            {
+                isDrawing = true;
+                pos.z = 0;
+
+                // Instantiate and set the current mesh generator on button down
+                currentMeshGenerator = Instantiate(meshGeneratorPrefab, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity);
+                lineRend = currentMeshGenerator.transform;
+            }
+            else
+            {
+                isTouching = false; // Stop drawing lines when player cant draw
+            }
         }
 
-        // GetMouseButton(0) works for both the mouse and touch 
-        if (Input.GetMouseButton(0))
+        if (isTouching)
         {
-            isDrawing = true;
-            pos = Input.mousePosition;
-            pos.z = 10; //because for some reason it sets the z position to 10
-            pos = Camera.main.ScreenToWorldPoint(pos);
-            lineRend.position = pos;
+            if (canDraw)
+            {
+                isDrawing = true;
+                pos = Input.mousePosition;
+                pos.z = 10; //because for some reason it sets the z position to 10
+                pos = Camera.main.ScreenToWorldPoint(pos);
+                lineRend.position = pos;
+            }
+            else
+            {
+                isTouching = false; // Stop drawing lines when player cant draw
+            }
         }
 
+        // Set values to false when player doenst touch the screen
         if (Input.GetMouseButtonUp(0))
         {
+            isTouching = false;
             isDrawing = false;
         }
     }
