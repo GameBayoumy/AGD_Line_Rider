bf951f4318461c6390e9eb4112409cc9e7269176
Sharif Bayoumy :3 <XilionHero@gmail.com>
2017-09-24 21:29:34 +0200

    [Merge] Draw resource and UI elements

--- AGD - Line Rider/Assets/Scripts/Player.cs
+++ AGD - Line Rider/Assets/Scripts/Player.cs
@@ -1,26 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
 	public float speed;
     public float maxResource;
+    public float drawResource;
     public Touch touch;
-    public Text uiText;
-    public Slider uiSlider;
 
-    [SerializeField]
-    private float drawResource;
     private Rigidbody2D rb2d;
+
 	// Use this for initialization
 	void Start()
 	{
 		rb2d = GetComponent<Rigidbody2D>();
         drawResource = maxResource;             // Set start draw resource to the max resource
-        uiSlider.maxValue = maxResource;        // Set max value in ui slider
-        uiSlider.value = drawResource;          // Set current ui value to draw resource
 	}
 
 	// Update is called once per frame
@@ -37,10 +30,6 @@ public class Player : MonoBehaviour
             if(drawResource < maxResource)
                 drawResource++;
         }
-
-        // Update UI elements
-        uiSlider.value = drawResource;
-        uiText.text = "Draw " + drawResource;
 
         // Stop drawing when resource is empty
         if(drawResource <= 0)
