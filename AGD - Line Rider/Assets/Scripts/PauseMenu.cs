7c769f8f9c9a0cf307dde56365a588684d8ff349
Jermaine Kanhai <jermaine.kanhai@hva.nl>
2017-09-25 03:39:40 +0200

    [Adds] Pause Menu with Resume, Return to Menu, and Quit options

---
+++ AGD - Line Rider/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+	public Transform canvas;
+	public Transform controls;
+	public Transform background;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Pause ();
+		}
+	}
+
+	public void Pause(){
+		if (canvas.gameObject.activeInHierarchy == false) {
+			canvas.gameObject.SetActive (true);
+			//Causes the game to freeze in place (pausing)
+			Time.timeScale = 0;
+			//Touch gameplay is turned off
+			controls.GetComponent<Touch>().enabled = false;
+			//Scrolling background is frozen
+			background.GetComponent<scroll>().enabled = false;
+			} else {
+			//Everything resumes once more
+			Resume();
+		}
+	}
+
+	public void Resume(){
+		canvas.gameObject.SetActive (false);
+		Time.timeScale = 1;
+		controls.GetComponent<Touch> ().enabled = true;
+		background.GetComponent<scroll> ().enabled = true;
+	}
+	
+	public void MainMenu(){
+		SceneManager.LoadScene(0);
+	}
+
+	public void Quit()
+	{			
+		Application.Quit();
+	}
+
+}
