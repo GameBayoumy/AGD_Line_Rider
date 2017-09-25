e456160f0858b66b4907ed4ca6c33fefc05e22ed
Sharif Bayoumy :3 <XilionHero@gmail.com>
2017-09-25 14:17:47 +0200

    [Fixes] Laser local scale. Laser now cuts off faster.

--- AGD - Line Rider/Assets/Scripts/LaserBehaviour.cs
+++ AGD - Line Rider/Assets/Scripts/LaserBehaviour.cs
@@ -3,29 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserBehaviour : MonoBehaviour {
-
-    public GameObject GameObjectLaser;
+    
     public Vector3 extendLaser;
     public EnemyLaser EnemyLaser;
-
-    private void Awake()
-    {
-        GameObjectLaser = GameObject.FindGameObjectWithTag("Laser");
-    }
-    // Use this for initialization
-    void Start () {
-    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         if (EnemyLaser.laserHit == true)
         {
-            GameObjectLaser.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - 0.02f,transform.localScale.z);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - 0.04f,transform.localScale.z);
         }
         else
         {
-            GameObjectLaser.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + 0.02f, transform.localScale.z);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + 0.04f, transform.localScale.z);
+        }
+
+        if(transform.localScale.y <= 0)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, .1f, transform.localScale.z);
         }
 	}
 }
