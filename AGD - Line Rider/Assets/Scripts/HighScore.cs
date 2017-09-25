2070741c5a024427a6594d883335dda9aca51443
Jermaine Kanhai <jermaine.kanhai@hva.nl>
2017-09-25 12:31:50 +0200

    [Updates] HighScore script

--- AGD - Line Rider/Assets/Scripts/HighScore.cs
+++ AGD - Line Rider/Assets/Scripts/HighScore.cs
@@ -10,6 +10,15 @@ public class HighScore : MonoBehaviour {
 	public Text timerText;
 	public Text resultText;
 
+	public GameOverMenu gameOverCall;
+	public GameObject gameOverCaller;
+
+	void Awake (){
+		//Calls the GameController which hosts the GameOverMenu script
+		GameObject gameOverCaller = GameObject.Find("GameController");     
+		gameOverCall = gameOverCaller.GetComponent<GameOverMenu>();
+	}
+
 	// Update is called once per frame
 	void Update () {
 
@@ -19,8 +28,15 @@ public class HighScore : MonoBehaviour {
 //			{
 //				rb.AddForce(0, jumpPower, 0);
 //			}
+
 		timeScore += Time.deltaTime;
 		timerText.text = timeScore.ToString("Score: 0");
 		resultText.text = timeScore.ToString ("Your Score: 0");
+
+		if(gameOverCall.gameOverState == true){
+			if (PlayerPrefs.GetFloat ("Highscore") < timeScore) {
+				PlayerPrefs.SetFloat ("Highscore", timeScore);
+			}
+		}
 	}
 }
