c787cedeadb419638c3cb30e90af7226e278cd30
Michael Rensen <michael@illi-engineering.nl>
2017-10-10 13:12:22 +0200

    Added: SplittingEnemy, This enemy splits into two smaller enemies. Bigger enemy can be spawned and deletes itself after two smaller enemies are instantiated.

---
+++ AGD - Line Rider/Assets/Scripts/EnemySplitting.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySplitting : MonoBehaviour {
+
+    private Vector3 PlayerPosition;
+
+    public GameObject Player;
+    public GameObject EnemySplittingSmallUp;
+    public GameObject EnemySplittingSmallDown;
+    public GameOverMenu gameOverCall;
+    public GameObject gameOverCaller;
+    bool SpawnAllowed = true;
+    bool spawned = false;
+
+    public GameObject Up;
+    public GameObject Down;
+
+    void Awake()
+    {
+        //Calls the GameController which hosts the GameOverMenu script
+        gameOverCaller = GameObject.Find("GameController");
+        gameOverCall = gameOverCaller.GetComponent<GameOverMenu>();
+        Player = GameObject.Find("Player");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerPosition = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z);
+
+        if((transform.position.x - PlayerPosition.x) <= 15 && SpawnAllowed == true){
+            spawned = true;
+            Up = Instantiate(EnemySplittingSmallUp, new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z), Quaternion.identity);
+            Down = Instantiate(EnemySplittingSmallDown, new Vector3(transform.position.x, transform.position.y - 0.2f, transform.position.z), Quaternion.identity);
+
+            SpawnAllowed = false;
+            Destroy(gameObject);
+        }
+
+        if (spawned == true)
+        {
+            //GameObject uppie = GameObject.Find("EnemySplittingSmallUp");
+           Up.GetComponent<Rigidbody2D>().AddForce(new Vector2(-60, 40));
+           Down.GetComponent<Rigidbody2D>().AddForce(new Vector2(-60, -40));
+        }
+	}
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+        if (collision.gameObject.tag == "Player")
+        {
+            gameOverCall.gameOverState = true;
+        }
+    }
+}
