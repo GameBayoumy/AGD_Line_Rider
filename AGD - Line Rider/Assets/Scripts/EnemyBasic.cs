ce23bc5e4c0d9be21979679b02c7a73eeab9de97
Lisa The <lisathe97@gmail.com>
2017-10-10 13:27:13 +0200

    [Add] Chasing enemy  - Redefine SpawnableGameObject methods in Enemy Basic

--- AGD - Line Rider/Assets/Scripts/EnemyBasic.cs
+++ AGD - Line Rider/Assets/Scripts/EnemyBasic.cs
@@ -4,24 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EnemyBasic : SpawnableGameObject {
-   
-   
-   
 
-	//// Use this for initialization
-	//void Start () {
-	////	GetComponent<ObjectPoolScript>();
-	//}
-	
-	//// Update is called once per frame
-	//void Update () {
-	//	//SpawnObject(EnemyBasicPool);
-	//}
+    protected override void Awake() { base.Awake(); }
+    protected override void Update() { base.Update(); }
+    protected override void DisableGameObject() { base.DisableGameObject(); }
 
-	private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
 	{   
-
-
 		if (collision.gameObject.tag == "Player")
 		{
 			SceneManager.LoadScene("LineMeshTest");
