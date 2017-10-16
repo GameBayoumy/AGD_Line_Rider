0f7927129c8807dd4b0d96156303b203da9ce7dd
Michael Rensen <michael@illi-engineering.nl>
2017-10-16 12:27:33 +0200

    [ADDED] Circle Bumper enemy/object.

---
+++ AGD - Line Rider/Assets/Scripts/Bumper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player"){
+
+            transform.localScale += new Vector3(0.02f, 0.02f, 0);
+           // transform.localScale -= new Vector3(0.05f, 0.1f, 0);
+        }
+    }
+}
