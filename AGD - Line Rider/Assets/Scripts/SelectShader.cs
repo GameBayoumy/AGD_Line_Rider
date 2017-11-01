48648dc63b0f426b80dbe8cab1217e335dff456a
TheDavid <david.mijdam@hva.nl>
2017-11-01 12:05:19 +0100

    [CREATE] Added a menu that lets you change the shader for the line. This is saved in the PlayerPrefs.

---
+++ AGD - Line Rider/Assets/Scripts/SelectShader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectShader : MonoBehaviour {
+
+    public int shaderID;
+    Button myButton;
+    UnlockShaders shaderManager;
+
+	// Use this for initialization
+	void Start () {
+
+        myButton = GetComponent<Button>();
+        myButton.onClick.AddListener(ChangeShader);
+        shaderManager = GameObject.Find("ShaderManager").GetComponent<UnlockShaders>();
+            
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ChangeShader()
+    {
+        PlayerPrefs.SetInt("ShaderID", shaderID);
+        shaderManager.SelectShader();
+    }
+}
---
+++ AGD - Line Rider/Assets/Scripts/UnlockShaders.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockShaders : MonoBehaviour {
+
+    public Material activeMaterial;
+    public Material[] possibleMaterials;
+    public GameObject skinUI;
+    HighScore score;
+
+	// Use this for initialization
+	void Start () {
+
+        score = GameObject.Find("GameController").GetComponent<HighScore>();
+
+        PlayerPrefs.SetInt("ShaderID", PlayerPrefs.GetInt("ShaderID"));
+        PlayerPrefs.SetInt("UnlockedRope", 0);
+        PlayerPrefs.SetInt("UnlockedBubbles", 0);
+        PlayerPrefs.SetInt("UnlockedWave", 0);
+
+        SelectShader();
+	}
+	
+	// Update is called once per frame
+	void Update () {    
+        
+		if ((int)score.timeScore == 100)
+        {
+            UnlockShader("UnlockedRope");
+        }
+
+	}
+
+    public void UnlockShader(string shaderKey)
+    {
+        PlayerPrefs.SetInt(shaderKey, 1);
+    }
+
+    public void SelectShader()
+    {
+        activeMaterial = possibleMaterials[PlayerPrefs.GetInt("ShaderID")];
+    }
+
+    public void RevealSkinUI()
+    {
+        skinUI.SetActive(true);
+    }
+
+    public void HideSkinUI()
+    {
+        skinUI.SetActive(false);
+    }
+}
