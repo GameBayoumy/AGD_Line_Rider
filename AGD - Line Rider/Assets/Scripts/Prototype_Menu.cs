e219c9545fdf24408d1292605fbaec0c3b252b11
Jermaine Kanhai <jermaine.kanhai@hva.nl>
2017-11-07 11:30:47 +0100

    [Adds] Prototype_Menu script

---
+++ AGD - Line Rider/Assets/Scripts/Prototype_Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Prototype_Menu : MonoBehaviour {
+
+	public void Prototype_1()
+	{
+		SceneManager.LoadScene(2);
+	}
+
+	public void Prototype_2()
+	{
+		SceneManager.LoadScene (3);
+	}
+
+	public void Prototype_3()
+	{
+		SceneManager.LoadScene (4);
+	}
+}
