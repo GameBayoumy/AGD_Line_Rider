863507c6537b8154d7f3d90f71c101f2a825ed63
TheDavid <david.mijdam@hva.nl>
2017-11-28 14:09:05 +0100

    [CREATE] Made a scene for the set editor with a scroll bar of objects. Right now, only a bumper can be spawned.

---
+++ AGD - Line Rider/Assets/Scripts/DragObjectEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObjectEditor : MonoBehaviour {
+
+    public GameObject relatedPrefab;
+    public Sprite relatedSprite;
+    public Color relatedColor;
+    public Vector2 relatedSize;
+    public GameObject placeholder;
+
+	// Use this for initialization
+	void Start () {
+
+        placeholder = GameObject.Find("Placeholder");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(gameObject.transform.name);
+        }
+
+    }
+
+    public void SpawnObject()
+    {
+        placeholder.transform.position = Vector2.zero;
+        placeholder.transform.localScale = relatedSize;
+        placeholder.GetComponent<SpriteRenderer>().sprite = relatedSprite;
+        placeholder.GetComponent<SpriteRenderer>().color = relatedColor;
+        placeholder.GetComponent<PlaceholderScript>().objectToSpawn = relatedPrefab;
+    }
+}
