6322f0bb476b5247c3cd7dd566fdd28f30c4f44c
Michael Rensen <michael@illi-engineering.nl>
2017-12-08 11:41:25 +0100

    perhaps working?

--- AGD - Line Rider/Assets/Scripts/Currency.cs
+++ AGD - Line Rider/Assets/Scripts/Currency.cs
@@ -12,8 +12,28 @@ public class Currency : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
-        CurrencyPoints = PlayerPrefs.GetInt("Player Currency");
-        PlayerPrefs.SetInt("Player Currency", CurrencyPoints);
-	}
+	//void Start () {
+ //       CurrencyPoints = PlayerPrefs.GetInt("Player Currency");
+ //       PlayerPrefs.SetInt("Player Currency", CurrencyPoints);
+	//}
+
+
+    void Start()
+    {
+        // this array should be filled before you can use EncryptedPlayerPrefs :
+        EncryptedPlayerPrefs.keys = new string[5];
+        EncryptedPlayerPrefs.keys[0] = "23Wrudre";
+        EncryptedPlayerPrefs.keys[1] = "SP9DupHa";
+        EncryptedPlayerPrefs.keys[2] = "frA5rAS3";
+        EncryptedPlayerPrefs.keys[3] = "tHat2epr";
+        EncryptedPlayerPrefs.keys[4] = "jaw3eDAs";
+
+
+        CurrencyPoints = EncryptedPlayerPrefs.GetInt("Player Currency");
+        EncryptedPlayerPrefs.SetInt("Player Currency", CurrencyPoints);
+       // Debug.Log(EncryptedPlayerPrefs.GetInt("Player Currency", -1));
+    }
+
+
+
 }
