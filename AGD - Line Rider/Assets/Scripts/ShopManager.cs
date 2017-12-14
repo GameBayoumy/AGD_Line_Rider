3ac6df0db3fc97e3912ee23f0aba06805bb26026
Jermaine Kanhai <jermaine.kanhai@hva.nl>
2017-12-14 13:16:57 +0100

    WIP ShopManager Script

---
+++ AGD - Line Rider/Assets/Scripts/ShopManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShopManager : MonoBehaviour {
+
+	//This script manages the shop of the game
+
+	public Text currentCurrencyText;
+
+	// Use this for initialization
+	void Start () {
+		
+		//This value within PlayerPrefs determines whether the product in question
+		//Has been bought (and therefore unlocked) or not
+		//0 means that it is locked, 1 means that it is unlocked
+		PlayerPrefs.SetInt ("Product_1_Key", 0);
+		PlayerPrefs.GetInt ("Product_1_Key");
+
+		PlayerPrefs.SetInt ("Product_2_Key", 0);
+		PlayerPrefs.GetInt ("Product_2_Key");
+	
+	}
+
+	//Gets the current currency from the game
+	public int getCurrency(){
+		return PlayerPrefs.GetInt ("Player Currency");
+	}
+
+	//Updates the current currency from the game
+	public void setCurrency(int value){
+		PlayerPrefs.SetInt ("Player Currency", value);
+	}
+
+	void Purchase(){
+		//Checks if currency is equal to or more than the price
+		//If it is, then the currentCurrency will have its value subtracted by the price
+		if (getCurrency() >= 200) {
+			setCurrency((getCurrency() - 200));
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Displays the current currency within the shop scene
+		currentCurrencyText.text = (getCurrency().ToString()) + " Coins";
+	}
+
+
+
+
+}
