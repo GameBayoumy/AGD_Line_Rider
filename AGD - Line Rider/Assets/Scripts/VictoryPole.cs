49946900e3ee98f25638a980a083418b8d0db213
Jermaine Kanhai <jermaine.kanhai@hva.nl>
2018-01-21 12:41:29 +0100

    [ADD] VictoryPole script

---
+++ AGD - Line Rider/Assets/Scripts/VictoryPole.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryPole : MonoBehaviour {
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.name == "pCylinder1")
+		{
+			VictoryScreenMenu.SetVictoryState(true);
+		}
+	}
+}
